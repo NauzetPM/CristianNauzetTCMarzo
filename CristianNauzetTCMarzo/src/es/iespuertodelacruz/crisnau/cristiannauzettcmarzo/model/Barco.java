b9fdad0ffd31d81c1723e683902b4cad5fb7ab42
NauzetPM <imnauzet2015@gmail.com>
2023-03-29 11:04:19 +0100

    feat(barco): Se ponen los atributos,se crea el constructor y se añaden getters , setters y toString #1

--- CristianNauzetTCMarzo/src/es/iespuertodelacruz/crisnau/cristiannauzettcmarzo/model/Barco.java
+++ CristianNauzetTCMarzo/src/es/iespuertodelacruz/crisnau/cristiannauzettcmarzo/model/Barco.java
@@ -9,5 +9,59 @@ package es.iespuertodelacruz.crisnau.cristiannauzettcmarzo.model;
  * @author Kaz
  */
 public class Barco {
+    private int tamanio;
+    private boolean tocado;
+    private boolean hundido;
+    private int vecesTocado;
+    /**
+     * Constructor con 1 parametro
+     * @param tamanio 
+     */
+    public Barco(int tamanio) {
+        this.tamanio = tamanio;
+        this.hundido=false;
+        this.tocado=false;
+        this.vecesTocado=0;
+    }
+    //Getters
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public boolean isTocado() {
+        return tocado;
+    }
+
+    public boolean isHundido() {
+        return hundido;
+    }
+
+    public int getVecesTocado() {
+        return vecesTocado;
+    }
+    //Setters
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setTocado(boolean tocado) {
+        this.tocado = tocado;
+    }
+
+    public void setHundido(boolean hundido) {
+        this.hundido = hundido;
+    }
+
+    public void setVecesTocado(int vecesTocado) {
+        this.vecesTocado = vecesTocado;
+    }
+
+    @Override
+    public String toString() {
+        return "Barco{" + "tamanio=" + tamanio + ", tocado=" + tocado + ", hundido=" + hundido + ", vecesTocado=" + vecesTocado + '}';
+    }
+    
     
 }
